4889e70dad8cbdcb4edc6e7d8792c2021bd78131
Er1cG1ao <eric15810870801@outlook.com>
2025-03-09 16:49:50 +0800

    trying optimizing, first try with caching all shits in at first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*.(svg|gif|png|jpg|jpeg|css|js|woff2|ttf)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable", // Cache for 1 year
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
